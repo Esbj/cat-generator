1e5eec3c0a1e485e6206ff40a3619319e11b630e
Alexander Esbjörnsson <alex.esbj@gmail.com>
2023-01-17 21:27:46 +0100

    WIP Cats log to the console

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,10 @@ const key =
   "live_Wg9vMl1FRnMcvYGi5AWc5F1Afxoax8QkjKjtZJW6eN0VI29W7OdLiDexi2P4Pth5";
 const baseUrl = "https://api.thecatapi.com/v1/images/search";
 
-
 let url =
   "https://api.thecatapi.com/v1/images/search?category_ids=4&api_key=live_Wg9vMl1FRnMcvYGi5AWc5F1Afxoax8QkjKjtZJW6eN0VI29W7OdLiDexi2P4Pth5";
 
-const categories = [
+const CATegories = [
   { id: 1, name: "hats" },
   { id: 2, name: "space" },
   { id: 4, name: "sunglasses" },
@@ -16,6 +15,31 @@ const categories = [
   { id: 15, name: "clothes" }
 ];
 
-const catRes = fetch(url).then((response) =>
-  response.json().then((data) => console.log(data))
-);
+const button = document.querySelector("button#cat-button") as HTMLButtonElement;
+const main = document.querySelector("main") as HTMLElement
+const imageEl = document.querySelector("image")
+
+
+
+button.addEventListener("click", async function (): Promise<void> {
+  
+  const catRes = fetch(url)
+  .then((response) =>
+    response.json()
+  .then(
+    function(data) {
+    return data
+    // imageEl.setAttribute("src", data[0].url)
+
+  })
+  );
+  console.log(await catRes)
+  
+});
+
+
+// function(data) {
+//   const imageEl = document.createElement("image")
+//   imageEl.setAttribute("src", data[0].url
+//   main.append(imageEl)
+// }
