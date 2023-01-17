a61d8cef53db3dfba805ac5c8237abe93bea8878
Alexander Esbjörnsson <alex.esbj@gmail.com>
2023-01-17 20:25:04 +0100

    Categories select working in JS and checkboxes added to HTML

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,21 @@
-const key = "live_Wg9vMl1FRnMcvYGi5AWc5F1Afxoax8QkjKjtZJW6eN0VI29W7OdLiDexi2P4Pth5"
-const url = "https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_Wg9vMl1FRnMcvYGi5AWc5F1Afxoax8QkjKjtZJW6eN0VI29W7OdLiDexi2P4Pth5"
+const key =
+  "live_Wg9vMl1FRnMcvYGi5AWc5F1Afxoax8QkjKjtZJW6eN0VI29W7OdLiDexi2P4Pth5";
+const baseUrl = "https://api.thecatapi.com/v1/images/search";
 
 
-const res = fetch(url).then(response => response.json().then(data => console.log(data)))
+let url =
+  "https://api.thecatapi.com/v1/images/search?category_ids=4&api_key=live_Wg9vMl1FRnMcvYGi5AWc5F1Afxoax8QkjKjtZJW6eN0VI29W7OdLiDexi2P4Pth5";
+
+const categories = [
+  { id: 1, name: "hats" },
+  { id: 2, name: "space" },
+  { id: 4, name: "sunglasses" },
+  { id: 5, name: "boxes" },
+  { id: 7, name: "ties" },
+  { id: 14, name: "sinks" },
+  { id: 15, name: "clothes" }
+];
+
+const catRes = fetch(url).then((response) =>
+  response.json().then((data) => console.log(data))
+);
